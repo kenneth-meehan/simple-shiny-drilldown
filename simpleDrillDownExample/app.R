4b203a6ee49a6664e6d1841034d48821aead1291
Kenneth Meehan <kenneth.meehan@hmhco.com>
2018-05-09 16:23:42 -0400

    allow variable number of schools and months, start making user-selectable

--- simpleDrillDownExample/app.R
+++ simpleDrillDownExample/app.R
@@ -1,16 +1,17 @@
 library(ggplot2)
 
 #Set up bogus dataframe:
-Months <- c(rep("Jan", 4), rep("Feb", 4), rep("Mar", 4), rep("Apr", 4), rep("May", 4))
-Schools <- rep(c("School A", "School B", "School C", "School D"),5) 
-Nitems <- c(0, 1016, 2001, 501, 666, 1962, 1999, 2019, 1776, 2018, 2525, 3087, 1018, 1891, 1511, 2617, 371, 678, 2191, 2216)
+Nmonths  <- 7
+Nschools <- 5
+maxNitemsPerSchoolPerMonth <- 2000
+month_names <- format(ISOdate(2018,1:12,1),"%b")[1:Nmonths] #names of first Nmonths months
+Months  <- rep(month_names, each=Nschools)
+Schools <- rep(paste("School",LETTERS[1:Nschools]), times=Nmonths)
+Nitems <- sample(0:maxNitemsPerSchoolPerMonth,Nmonths*Nschools,replace=T)
 df <- as.data.frame(cbind(Months, Schools, Nitems))
-rm(Months)
-rm(Schools)
-rm(Nitems)
+rm(Months, Schools, Nitems)
 df$Nitems <- as.integer(as.character(df$Nitems))
-df$Months <- factor(df$Months, levels=c("Jan", "Feb", "Mar", "Apr", "May"))
-
+df$Months <- factor(df$Months, levels=month_names)
 
 
 #Define user interface:
@@ -23,14 +24,17 @@ ui <- fluidPage(
     column(width = 2,
            wellPanel(
              radioButtons("abscissa", "x-axis on left graph",
-                          c("Months" = "Months",
-                            "Schools" = "Schools"),
-                          selected="Months")
+                          c("Schools" = "Schools",
+                            "Months" = "Months"
+                            ),
+                          selected="Schools"),
+             sliderInput("Nschools", "# Schools",
+                         min = 2, max = 12,
+                         value = 5)
            )
     ),
-
     column(width = 10, class = "well",
-           h5("Click on left plot to drill down."),
+           h5("Click on first plot to drill down."),
            fluidRow(
              column(width = 6,
                     plotOutput("plot1", height = 300,
@@ -78,9 +82,9 @@ server <- function(input, output) {
       xcuts <- seq(0.5, nbars+0.5, 1)                    #x-values of boundaries bewteen bars' click zones
       ytops <- rep(0, nbars)                             #max y values for each bar; initialize each to 0
       for (bar in 1:nbars){
-       ytops[bar] <-  sum(df[df[, p()$labels$x]==levels(p()$data[, p()$labels$x])[bar], p()$labels$y]) #add up all y-values for this bar. CONVOLUTED!
+       ytops[bar] <-  sum(df[df[, p()$labels$x]==levels(p()$data[, p()$labels$x])[bar], p()$labels$y]) #add up all y-values for this bar. Sleek!
        if(cx>xcuts[bar] & cx<=xcuts[bar+1] & cy<=ytops[bar]){
-         assign(p()$labels$x, levels(p()$data[, p()$labels$x])[bar]) #now more generic! but now we set schools, not school...
+         assign(p()$labels$x, levels(p()$data[, p()$labels$x])[bar])
          break                                                       #leave loop once you've found where the click was
        }
       }
@@ -98,7 +102,6 @@ server <- function(input, output) {
         y_pos <- 0.8*ylims[2] #80% of way up
         dfs <- df[df$Months==Months,]
         ggplot(dfs, aes(x=Schools, y=Nitems, group=1)) +
-          #geom_line(size=2) + geom_point(size=5, color='goldenrod') +
           geom_bar(stat = "identity", fill='goldenrod') +
           coord_cartesian(ylim=ylims) +  #want same y-scale as 1st graph
           annotate("text", label=Months, x=x_pos, y=y_pos, size=10)
